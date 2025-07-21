35bcab91909b2da05f4f8f5d734b114c9ff83ac6
Adilson Quifucussa <adilson.quifucussa@tis.ao>
2025-07-21 20:59:22 +0100

    tag de verificação da google play console

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/trumunus-site',
+  assetPrefix: '/trumunus-site/',
+  trailingSlash: true,
 };
 
 export default nextConfig;
