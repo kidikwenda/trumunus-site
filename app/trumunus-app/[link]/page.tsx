a42991998c2340853aa57bd3813a5825addb4515
Adilson Quifucussa <adilson.quifucussa@tis.ao>
2025-08-30 02:44:04 +0100

    alterar query params para somente params e manter url encriptada

--- app/trumunus-app/[link]/page.tsx
+++ app/trumunus-app/[link]/page.tsx
@@ -1,15 +1,16 @@
 "use client";
 import { useEffect, useState } from "react";
-import { useSearchParams } from "next/navigation";
+import { useParams } from "next/navigation";
 
 export default function RedirectPage() {
-  const searchParams = useSearchParams();
+  const { link } = useParams<{ link: string }>();
   const [deepLink, setDeepLink] = useState<string | null>(null);
 
   useEffect(() => {
-    const paramLink = searchParams.get("link") || "trumunus://open";
+    // monta o deep link usando o parâmetro de rota
+    const paramLink = Buffer.from(link, 'base64').toString('utf8') || "trumunus://open";
     setDeepLink(paramLink);
-  }, [searchParams]);
+  }, [link]);
 
   useEffect(() => {
     if (!deepLink) return;
@@ -17,7 +18,7 @@ export default function RedirectPage() {
     try {
       const userAgent = navigator.userAgent || navigator.vendor;
 
-      const androidLink ="https://play.google.com/store/apps/details?id=com.kidikwenda.trumunus";
+      const androidLink = "https://play.google.com/store/apps/details?id=com.kidikwenda.trumunus";
       const iosLink = "https://apps.apple.com/app/idSEU_APP_ID";
       const webLink = window.location.origin;
 
@@ -42,10 +43,9 @@ export default function RedirectPage() {
     }
   }, [deepLink]);
 
-return (
-  <div className="flex items-center justify-center min-h-screen bg-[#0A0E21]">
-    <div className="w-16 h-16 border-4 border-white border-t-transparent rounded-full animate-spin"></div>
-  </div>
-);
-
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-[#0A0E21]">
+      <div className="w-16 h-16 border-4 border-white border-t-transparent rounded-full animate-spin"></div>
+    </div>
+  );
 }
